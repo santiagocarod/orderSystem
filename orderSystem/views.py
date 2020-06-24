bcadf74019e84492598f7cd5b06e1da1d9741cd9
Santiago Caro <santiagocaroduque@gmail.com>
2020-06-24 18:13:25 -0500

    Redirect on index if logged in

--- orderSystem/views.py
+++ orderSystem/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
 def index (request):
+    if request.user.is_authenticated:
+       return redirect("appindex")
     return render(request,'home/index.html')
 
 @login_required(login_url='/accounts/login/')
